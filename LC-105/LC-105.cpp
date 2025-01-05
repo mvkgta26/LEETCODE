86194b6cc4d9dc04fff79f55d3d9dc3c04d8c266
Venkata Krishna Mangapuram <krishna@Mac.lan>
2025-01-05 13:33:27 -0500

    Optimised Working Version V1

--- LC-105/LC-105.cpp
+++ LC-105/LC-105.cpp
@@ -27,23 +27,26 @@ map<int,int> inorder_hash_func(vector<int>& inorder)
     return inorder_hash;
 }
 
-TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder, map<int, int> &inorder_hash)
+// The function is a recursive function that takes the inorder and preorder of a tree,
+// finds the head of the tree, builds left and right subtrees of the head, and attaches them to the head.
+// Finally returns this head of the tree.
+// in_start and in_end mark the boundaries (the start and end indices) within inorder[] for the current subtree in the resursive call
+// Similarly pre_start and pre_end.
+// Imagine as if you are passing only the preorder and inorder subarrays relevant to the subtree for the recursive call. But instead of passing the subarray itself, we pass the boundaries of the subarray within the original pre_order and in_order.
+TreeNode* buildTreeHelper(vector<int>& preorder, int pre_start, int pre_end, vector<int>& inorder, int in_start, int in_end, map<int, int> &inorder_hash)
 {
-    // Exit Condition 1:
-    if(preorder.size() == 1)
+    // Exit Condition 1: If there is only one element in the preorder array for the current subtree, then it is a leaf node
+    if(pre_start == pre_end-1)
     {
-        TreeNode* node =  new TreeNode(preorder[0]);
-        node->left = NULL;
+        TreeNode* node =  new TreeNode(preorder[pre_start]);
+        node->left = NULL;    //Leaf nodes should have NULL left and right children.
         node->right = NULL;
         return node;
     }
 
-
-    int head_node_val = preorder[0];
-    TreeNode* head_node = new TreeNode(head_node_val);
-
-
-    int left_subtree_length = inorder_hash[head_node_val];
+    // int head_node_val = preorder[0];
+    TreeNode* head_node = new TreeNode(preorder[pre_start]);
+    int left_subtree_length = inorder_hash[preorder[pre_start]] - in_start;    // pre_start marks the start of the preorder array pertaining to the current subtree in the recursive call. The first element in the preorder for the current subtree will be the head node.
 
 
     TreeNode* left_subtree_head = NULL;
@@ -55,14 +58,14 @@ TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder, map<int, 
     }    
     
     else
-    {    // Create preorder and inorder arrays for the left subtree. Build the left subtree and get the left subtree head.
-        vector<int> left_subtree_inorder (inorder.begin(), inorder.begin() + left_subtree_length);
-        vector<int> left_subtree_preorder (preorder.begin() + 1, preorder.begin() + left_subtree_length + 1);
-        left_subtree_head = buildTree(left_subtree_preorder, left_subtree_inorder);
+    {   // Create preorder and inorder arrays for the left subtree. Build the left subtree and get the left subtree head.
+        //vector<int> left_subtree_inorder (inorder.begin(), inorder.begin() + left_subtree_length);
+        //vector<int> left_subtree_preorder (preorder.begin() + 1, preorder.begin() + left_subtree_length + 1);
+        left_subtree_head = buildTreeHelper(preorder, pre_start + 1, pre_start + left_subtree_length + 1,  inorder, in_start, in_start + left_subtree_length, inorder_hash);
     }  
 
     // Handle edges cases if there is only left subtree to the current head node (Asymmetry)
-    if (left_subtree_length + 1 >= inorder.size())  // left subtree and the head node make up the whole inorder of the tree.
+    if ( in_start + left_subtree_length + 1 >= in_end)  // left subtree and the head node make up the whole inorder of the tree.
     {   
         right_subtree_head = NULL;
     }
@@ -70,9 +73,9 @@ TreeNode* buildTreeHelper(vector<int>& preorder, vector<int>& inorder, map<int, 
     else
     {    
         // Create the preorder and inorder arrays for the right subtree. Build the right subtree and get the right subtree head.
-        vector<int> right_subtree_inorder (inorder.begin() + left_subtree_length + 1, inorder.end());   // Middle/head_node + 1 --> right end 
-        vector<int> right_subtree_preorder (preorder.begin() + left_subtree_length + 1, preorder.end());    // The right subtree section
-        right_subtree_head = buildTree(right_subtree_preorder, right_subtree_inorder);
+        //vector<int> right_subtree_inorder (inorder.begin() + left_subtree_length + 1, inorder.end());   // Middle/head_node + 1 --> right end 
+        //vector<int> right_subtree_preorder (preorder.begin() + left_subtree_length + 1, preorder.end());    // The right subtree section
+        right_subtree_head = buildTreeHelper(preorder, pre_start + left_subtree_length + 1, pre_end, inorder, in_start + left_subtree_length + 1, in_end, inorder_hash);
     }  
 
     // Attach the left and right subtree heads to the current head node.
@@ -87,7 +90,9 @@ TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
 
     map<int, int> inorder_hash = inorder_hash_func(inorder);
 
-    TreeNode* root = buildTreeHelper(preorder, inorder, inorder_hash);
+    // Make the call to the recursive helper function
+    // Initialy the boundaries of preorder and inorder are the whole arrays itself, because the recrusive call is on the entire tree.
+    TreeNode* root = buildTreeHelper(preorder, 0, preorder.size(), inorder, 0, inorder.size(), inorder_hash);
 
     return root;
 }
