c8d33ba1dc7c13cbbba81a671c2e2c617e955e6b
Venkata Krishna Mangapuram <mvkgta26@gmail.com>
2025-01-18 18:11:38 -0500

    LC-973 Attempt 1

---
+++ LC-973/LC-973.cpp
@@ -0,0 +1,65 @@
+#include <queue>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int calcDist(vector<int> coordinates)
+{
+    int x = coordinates[0];
+    int y = coordinates[1];
+
+    int sum = x*x + y*y;
+    int dist = sqrt(sum);
+
+    return dist;
+}
+
+vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
+{
+
+    // Create a priority queue that sorts the vector<> based on the int key value
+    // Priority queue syntax: std::priority_queue<ElementType, ContainerType, ComparatorType> pq;
+    // Here the first input: Element Type: {int, vector<>} pair
+    // The second input : vector< pair<int, vector<int>> > mentions that the container used to implement the priority queue is a vector which holds our pairs
+    // There is no third input here, because the default is taken as less<>, which will make the pqueue as a maxheap
+    priority_queue < pair< int, vector<int> >, 
+                    vector< pair<int, vector<int>> >
+                    > 
+                    pque;
+
+
+    vector<vector<int>> outputs;
+
+    for (int i=0; i<points.size(); i++)
+    {
+        vector<int> coordinate = points[i];
+
+        int dist = calcDist(coordinate);
+
+        pque.push({dist, coordinate});
+        
+        // If there are more than k elements in the priority que, pop off the max element from the priority que
+        if (pque.size() > k)
+        {
+            pque.pop();
+        }
+    }
+    
+    // Empty the values from the priority queue into a vector
+    while (!pque.empty())
+    {
+        pair output = pque.top();
+        outputs.push_back(output.second);
+    }
+    
+    return outputs;
+}
+
+
+int main()
+{   
+       
+    return 1;
+}
