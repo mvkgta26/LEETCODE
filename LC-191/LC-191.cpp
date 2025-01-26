ea37d82f59747f1c0860ee8462a59b51f5000c8c
Venkata Krishna Mangapuram <mvkgta26@gmail.com>
2025-01-25 19:12:17 -0500

    LC-191 final with notes

--- LC-191/LC-191.cpp
+++ LC-191/LC-191.cpp
@@ -20,7 +20,17 @@ int hammingWeight(int n)
 }
 */
 
+/*
+Proof for why n & (n-1) will set the least significant 1 in the bit representation to 0, and leave everything else untouched
+When you do n='b31 b30 .... bk=1 0 ... 0 0' - 1 :
+    All the bits lower than bk are 0's, bk is the first 1
+    When you subtract by 1, all the 0's before bk will become 1, and bk will become 0 (since it is the first 1, it will be carried to the right)
+    Now the number n-1 is: b31 b30 ..... bk=0 1 1 ...1
+    Hence, n & (n-1) : b31 b30 .... bk=0 0 0 .. 0, which is just n but with the least significant 1 turned to 0
+*/
 
+
+ 
 int hammingWeight(int n) 
 {
     int count = 0;
