fc4549c2af6c53c9d168387e0fb44c7db61d2c2d
Venkata Krishna Mangapuram <mvkgta26@gmail.com>
2025-07-17 10:45:49 -0400

    LC-695 Commenting out visited array implementation

--- LC-695/LC-695.cpp
+++ LC-695/LC-695.cpp
@@ -6,51 +6,32 @@ using namespace std;
 
 // DFS iteration over the grid, traverse the entire current island and count the number of blocks in the island. 
 // Only dummy return value, the island area is counted using a static variable.
-int DFS(vector<vector<int>>& grid, int& m, int& n, int i, int j, int& island_area, vector<vector<int>>& visited)
+int DFS(vector<vector<int>>& grid, int& m, int& n, int i, int j, int& island_area) //, vector<vector<int>>& visited)
 {
-	visited[i][j] = 1;
-
 	// If current block is a land block, count it for island_area
-	if (grid[i][j] == 1)
+	// Check if there is an univisited land block in the grid in the position i, j
+	if ( (i>=0) && (i<m) && (j>=0) && (j<n) && (grid[i][j] == 1) ) // && (visited[i][j]==0)
 	{
 		island_area++;
+		//visited[i][j] = 1;
+		grid[i][j] = 0; // Make it as 0 to ensure that you dont count this again
+
+		DFS(grid, m, n, i, j-1, island_area); //, visited);  // Left
+		DFS(grid, m, n, i, j+1, island_area); //, visited);  // Right
+		DFS(grid, m, n, i+1, j, island_area); //, visited);  // Down
+		DFS(grid, m, n, i-1, j, island_area); //, visited);  // Up
 	}
 	
-	// Check the left element. Call DFS if left element is an unvisited land
-	if ( (j-1)>=0 && visited[i][j-1] == 0 && grid[i][j-1] == 1 )
-	{
-		DFS(grid, m, n, i, j-1, island_area, visited);
-	}
-	
-	// Check the right element. Call DFS if right element is an unvisited land
-	if ( (j+1)<n && visited[i][j+1] == 0 && grid[i][j+1] == 1 )
-	{
-		DFS(grid, m, n, i, j+1, island_area, visited);
-	}
-
-	// Check the down element. Call DFS if down element is an unvisited land
-	if ( (i+1)<m && visited[i+1][j] == 0 && grid[i+1][j] == 1 )
-	{
-		DFS(grid, m, n, i+1, j, island_area, visited);
-	}
-
-	// Check the up element. Call DFS if up element is an unvisited land
-	if ( (i-1)>=0 && visited[i-1][j] == 0 && grid[i-1][j] == 1 )
-	{
-		DFS(grid, m, n, i-1, j, island_area, visited);
-	}
-
 	return 0;	// Dummy return
 }
-
-
 
 int maxAreaOfIsland(vector<vector<int>>& grid) 
 {
 	int m = grid.size();	// Num rows
 	int n = grid[0].size();		// Num columns
 	
-	vector<vector<int>> visited(m, vector<int>(n, 0));	// 2D visited array of [m,n] dimensions. To mark if block visited during DFS
+	//vector<vector<int>> visited(m, vector<int>(n, 0));	// 2D visited array of [m,n] dimensions. To mark if block visited during DFS
+
 	
 	int max_island_area = -1;
 	
@@ -61,23 +42,14 @@ int maxAreaOfIsland(vector<vector<int>>& grid)
 		{
 			int island_area = 0;
 			
-			// If you encounter an unvisited land block, initiate DFS to traverse over the entire land surrounding it
-			if (visited[i][j] == 0 && grid[i][j] == 1)
-			{
-				DFS(grid, m, n, i, j, island_area, visited);
-			}
-			
-			// Max check
-			if (island_area > max_island_area)
-			{
-				cout << "Island Area: " << island_area << "\n";
-				max_island_area = island_area;
-			}
+			DFS(grid, m, n, i, j, island_area); //, visited);
+			max_island_area = max(island_area, max_island_area);    // If current island_area is the max
 		}
 	}
 	
 	return max_island_area;
 }
+
 
 int main()
 { 
