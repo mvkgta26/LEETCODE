52f6233d3592a677a40d937a832631c4bbdb42d3
mvkgta26 <mvkgta26@gmail.com>
2025-12-27 23:16:34 +0530

    LC-684 Commented Final

--- LC-684/LC-684.cpp
+++ LC-684/LC-684.cpp
@@ -1,3 +1,23 @@
+/*
+
+REFER ONENOTE FOR DETAILS
+
+Algorithm:
+	1. Perform DFS and return the first cycle node that you encounter.
+		a. As you perform DFS, keep track of the parent of each node in their respective DFS paths.
+			parent[] array: parent[i] = j, means the DFS call to the node-i was called from the node-j. 
+			
+	2. If we back track the DFS path from this cycle node back to itself, using the parent[] array, that will give us ALL THE NODES WITHIN 
+	THE CYCLE IN THE GRAPH. (We know a DFS path went from this node, to this node again cyclically, because we visit an already visited vertex during our DFS traversal)
+
+	3. Hash all the Cycle nodes.
+	
+	4. Iterate the edges[] from right to left. Edges that have both nodes as cycle nodes, will be cycle edges. (Use cycle node hash for this)
+		Remove the first cycle edge that you encounter. 
+		This will be the solution: The right most edge that can be removed to make the graph acyclic, and hence a tree.
+*/
+
+
 #include <iostream>
 #include <vector>
 
@@ -16,9 +36,11 @@ int dfs_helper(int node, vector<vector<int>>& adj_list, vector<int>& visited, ve
 		// Recrursive DFS call on unvisited neighbor
 		if (visited[nbr_node] == 0)
 		{
-			parent[nbr_node] = node;		// Note down the parent node
+			parent[nbr_node] = node;		// Store the parent node
 			
-			// If cycle was detected, immediate return 1    ********** Read
+			// If the child in the branch path detected a cycle, it will return 1.
+			// If cycle node is detected in this branch path from this node, immediately return 1. All we need is 1 cycle node.
+			// You don't have to look for all the other branch paths going from all the other neighbors of this node. 
 			if (dfs_helper(nbr_node, adj_list, visited, parent, cycle_start_node))	
 				return 1;	
 		}
@@ -41,15 +63,15 @@ int dfs_helper(int node, vector<vector<int>>& adj_list, vector<int>& visited, ve
 		}
 	}
 	
-	return 0;	// Dummy Return
+	return 0;	// return 0 if no cycle is found in this branch path
 }
 
 
 
 vector<int> findRedundantConnection(vector<vector<int>>& edges) 
 {
-	// Number of nodes in the graph = Number of edges in the graph , if there is one cycle in the graph
-	// Why??
+	// Number of nodes in the graph = Number of edges in the graph , in the cyclic graph in question
+	// Number of edges in a tree is always n-1
 	int n = edges.size();	
 	
 	// Convert edges to adjacency list (nodes are 1-indexed, so we need n+1 size)
@@ -60,7 +82,7 @@ vector<int> findRedundantConnection(vector<vector<int>>& edges)
 		adj_list[edges[i][1]].push_back(edges[i][0]);  // Undirected graph - add both directions
 	}
 	
-	
+	// Index 0 is dummy because there is no node 0
 	vector<int> parent(n + 1, -1);	// parent[] array: parent[i] = j, means the DFS call to the node-i was called from the node-j. Used to trace DFS path
 	vector<int> visited(n + 1, 0);
 	
@@ -69,16 +91,11 @@ vector<int> findRedundantConnection(vector<vector<int>>& edges)
 	visited[src] = 1;
 	dfs_helper(src, adj_list, visited, parent, cycle_start_node);	// DFS call from the source node. Get the value of cycle_start_node and parent[] array
 
-
-	
-	// Iterate the edges array from right to left. (The question asks for the edge that occurs rightmost and which when removed will make it acyclic)
-	// Find the first edge that has both nodes in the cycle. This will be an edge within the cycle
-	// This edge will be the solution
-	// Removing any edge from a cycle will make it NOT A CYCLE 
 	
 	// Prepare a hash map for cycle nodes. All the nodes in the parent[] array are the cycle nodes
 	vector<int> cycle_node_hash(n + 1, 0);	// cycle_node_hash[i] == 1, if the node 'i' is a cycle node
 	
+	// Prepare cycle_node_hash[]
 	// Iterate all the nodes of the cycle starting from the cycle_start_node, till we reach the cycle_start_node again. Using parent[] map	
 	// Mark all the nodes in the cycle in the cycle_node_hash[]
 	int node = cycle_start_node;
