82ade6dc48e4ae5d53e075d067cd1b5608306a85
mvkgta26 <mvkgta26@gmail.com>
2025-12-30 12:25:31 +0530

    Working solution v1'

---
+++ LC-435/LC-435.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+
+using namespace std;
+
+bool cmp(vector<int>& a, vector<int>& b)
+{
+	return a[1] < b[1];
+}
+
+
+// EFF: Earliest Finish First algorithm
+int eraseOverlapIntervals(vector<vector<int>>& intervals) 
+{
+	if (intervals.size() == 1)
+	{
+		return 0;
+	}
+
+	// Sort the intervals[][2] vector based on the element 2 in each of the inner vectors
+	sort(intervals.begin(), intervals.end(), cmp);
+	
+	int count = 0;
+	int last_added_interval_index = 0;
+	int i = 1;
+	while( i < intervals.size() )
+	{
+		int current_interval_start_time = intervals[i][0];
+		int prev_interval_end_time = intervals[last_added_interval_index][1];
+		
+		// Current interval overlaps with the previously added one: Remove this interval from the interval schedule
+		if (current_interval_start_time < prev_interval_end_time)
+		{
+			count++;	// Count this interval to be removed
+		}
+		
+		// Current interval does not overlap with the previously added one: Add this to the final interval schedule
+		else
+		{
+			last_added_interval_index = i;	
+		}
+			
+		i++;
+	}
+	
+	return count;
+}
+
+
+
