448effd6b5baa741d0f5a9d8c670e35c4f7c485c
mvkgta26 <mvkgta26@gmail.com>
2026-02-16 12:52:56 -0500

    LC-153.cpp done and commented

--- LC-153/LC-153.cpp
+++ LC-153/LC-153.cpp
@@ -1,3 +1,19 @@
+/*
+	Claims:
+		1. Inflection Point:  nums[min_index-1] < nums[min_index] < nums[min_index+1] .
+								min_index is the index of the minimum number in nums[]
+								nums[min_index] is the minimum number in nums[]
+								Proof: Obvious. Just write a generalized rotated sorted array and see.
+		1. If nums is rotated < n times, and If nums[index] >= nums[0] : The minimum element will be to the right of index
+		2. If nums is rotated < n times, and If nums[index] < nums[0] : The minimum element will be to the left of index
+								[Proof for both above: Pretty obvious. Just write a generalized rotated sorted array and see.]
+		
+	Corner Cases: 	
+		1. The nums[] is rotated n times. nums[] will be in proper sorted order. 
+				Only In that case, nums[n-1] > nums[0]
+		2. Left overflow and right overflow when checking for inflection point condition
+*/
+
 #include <vector>
 
 
@@ -36,7 +52,7 @@ int findMin(vector<int>& nums)
 			left = mid + 1;
 		}
 		
-		// The min element will be somwhere to the right of mid
+		// The min element will be somwhere to the left of mid
 		else if ( nums[mid] < nums[0] )
 		{
 			right = mid - 1; 
