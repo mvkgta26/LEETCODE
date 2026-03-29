db0d444901d467210813c7bd49b38fc54a2aad3e
mvkgta26 <mvkgta26@gmail.com>
2026-03-28 20:05:55 -0400

    LC-621.cpp fully completed and commented

--- LC-621/LC-621.cpp
+++ LC-621/LC-621.cpp
@@ -1,3 +1,18 @@
+/*
+
+	Main Greedy Idea: At any time slot, out of all the eligible tasks, choose the task with the highest number of task-occurrences/frequency left. PROOF: Not doing. Intuition.
+
+	Algorithm:
+		1. Hash the frequency of all the tasks 
+		2. Push all the frequencies to a max-heap priority queue. Invariant: Each element in the max-heap represents a task that is eligible to run in the current slot. (Location of Invariant after the innter while loop where the cooldown queue is unloaded into pq). PROOF: Not doing. Intuition.
+		3. Pop all the tasks that are eligible to run in the current slot from the cooldown_queue into pq
+		4. Now, Pop the top of the pq: This will be the task with highest frequency at the moment. Choose to run this in this slot. (If there is nothing in the pq, and the cooldown queue is full, that means this will have to be an idle slot.)
+			4.1. Subtract 1 from the frequency of this currently chosen task. This is because it has been executed once in this slot.
+			4.2. Push the next eligible slot for this task into the cooldown queue. (Invariant: The cooldown queue represents all tasks that are waiting for their next respective slots, in the increasing order of their next eligible slot from front to back.) (Invariant position after the inner while loop) PROOF: Not doing. Intuition.
+		5. When the priority queue is empty and the cooldown queue is empty, that means all the tasks completed running all their frequencies. END. Return the number of slots so far.
+*/
+
+
 #include <vector>
 #include <queue>
 #include <utility>
@@ -10,7 +25,7 @@ int leastInterval(vector<char>& tasks, int n)
 {	
 	int num_tasks = tasks.size();
 	
-	// Hash map for frequency of each task
+	// Hash frequency of each task
 	unordered_map<char, int> freq;
 	for (int i=0; i < num_tasks; i++)
 	{
@@ -23,8 +38,7 @@ int leastInterval(vector<char>& tasks, int n)
 	// Iterate the freq hash map and push each element to the priority que
 	for ( auto f = freq.begin(); f != freq.end(); f++ )
 	{
-		// Make {frequency_count, task-name} pair and push to max-heap pq
-		pair<int, char> curr_pair = {f->second, f->first};
+		// Push the frequencies of all tasks to the priority queue 'pq'
 		pq.push(f->second);
 	}
 	
@@ -35,7 +49,7 @@ int leastInterval(vector<char>& tasks, int n)
 	
 	while ( !pq.empty() || !cooldown_que.empty() )
 	{
-		// If the tasks executed last time are eligible are to run again, pop then off from the cooldown queue and push into the priority queue
+		// If the tasks executed last time are eligible are to run again, pop then off from the cooldown queue and push their frequencies into the priority queue
 		while ( !cooldown_que.empty() && curr_slot >= cooldown_que.front().second )
 		{
 			pq.push( cooldown_que.front().first );	
@@ -45,15 +59,13 @@ int leastInterval(vector<char>& tasks, int n)
 		// If the priority queue is empty, you cannot execute any task in this time slot. It will be an idle slot
 		if ( !pq.empty() )
 		{
-			//char curr_chosen_task = pq.top().second;
 			int curr_chosen_task_freq = pq.top();
-			
 			pq.pop();	// Pop the chosen task from the pq
 			
 			curr_chosen_task_freq--;	// Since the current task is now executed once, reduce its freq
 			
 			// Current task is executed in this cycle. Push it to the cooldown queue
-			// Push the current task to the cooldown queue: {new frequency of this task, the next slot when it is eligible to execute again}
+			// Push the current task to the cooldown queue: {new frequency of this task, the next slot when it is eligible to execute again} . The next-eligible-slot =  (curr_slot + n + 1) because there has to be a gap of n slots between current slot and the next elgible slot for this task
 			if ( curr_chosen_task_freq != 0 )	// If the current task is done executing all its occurrences, then it is completed. Dont add to the cooldown queue.
 			{
 				cooldown_que.push(make_pair(curr_chosen_task_freq, curr_slot + n + 1));		
@@ -62,7 +74,6 @@ int leastInterval(vector<char>& tasks, int n)
 		
 		curr_slot++;
 	}
-	
 	
 	return curr_slot;
 }
