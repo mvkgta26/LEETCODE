b86afd1cb2ccf941d67c06bbb75916873963d08b
mvkgta26 <mvkgta26@gmail.com>
2026-04-25 17:53:35 -0400

    LC-1116.cpp completed latest version with comments

--- LC-1116/LC-1116.cpp
+++ LC-1116/LC-1116.cpp
@@ -1,3 +1,10 @@
+/*
+	Termination:
+		// Zero thread will be the first thread to run when curr_num > n. All others wait at that point.
+		// Once the zero thread terminates, it wakes up all the other threads, who all go on to execute their termination code 
+*/
+
+
 #include <functional>
 #include <mutex>
 #include <condition_variable>
@@ -24,15 +31,20 @@ class ZeroEvenOdd
 		// printNumber(x) outputs "x", where x is an integer.
 		void zero(function<void(int)> printNumber) 
 		{
+			// Keep looping till curr_num > n
 			while(1)
 			{
 				unique_lock<mutex> lock (m);
 				
-				while (  curr_num <= n && zero_state == 0 )
+				// zero thread will be the first thread to run when curr_num > n. All others wait at that point first.
+					// Once the zero thread terminates, it wakes up all the other threads, who all go on to execute their termination code 
+				// while ( curr_num <= n && zero_state == 0 )
+				while ( zero_state == 0 )
 				{
 					cv.wait(lock);	
 				}
 				
+				// Termination: Terminate when all n is done
 				if (curr_num > n)	
 				{
 					cv.notify_all();
@@ -54,14 +66,16 @@ class ZeroEvenOdd
 			{
 				unique_lock<mutex> lock (m);
 				
+				// curr_num <= n : Dont sleep if finished guard. If it sleeps now, the zero thread will not raise a signal because it itself would be sleeping. It will keep sleeping indefinitely.
 				while ( curr_num <= n && (zero_state == 1 || curr_num % 2 != 0) )
 				{
 					cv.wait(lock);
 				}
 				
+				// Termination: Terminate when all n is done, signl other threads so that they wake up and terminate
 				if (curr_num > n)	
 				{
-					cv.notify_all();
+					// cv.notify_all();		// Not neeed, the even and odd threads both get woken by zero thread that runs first when curr_num > n
 					return;
 				}
 				
@@ -81,15 +95,17 @@ class ZeroEvenOdd
 			{
 				unique_lock<mutex> lock (m);
 				
+				// curr_num <= n : Dont sleep if finished guard. If it sleeps now, the zero thread will not raise a signal because it itself would be sleeping. It will keep sleeping indefinitely.
 				// Wait if current number is not odd
 				while ( curr_num <= n && ( zero_state == 1 || curr_num % 2 != 1 ) )
 				{
 					cv.wait(lock);
 				}
 				
+				// Termination: Terminate when all n is done
 				if (curr_num > n)	
 				{
-					cv.notify_all();
+					// cv.notify_all();		// Not neeed, the even and odd threads both get woken by zero thread that runs first when curr_num > n
 					return;
 				}
 				
