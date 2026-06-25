8a744e357f18181a0fdf90e8545f6c240dcb511b
mvkgta26 <mvkgta26@gmail.com>
2026-06-25 16:08:53 -0400

    LC-66.cpp completed and commented

--- LC-66/LC-66.cpp
+++ LC-66/LC-66.cpp
@@ -1,8 +1,61 @@
+/*
+	Concept:
+		Input-Num: 23458999999
+		
+		9-trail-group: The trail of 9's at the right end
+		Preceding-digit: The first digit from the right that is not 9. The first digit to the left of the 9-trail-group
+		
+		Claim: Carry gets passed only as long as there is a trail group of 9. 
+				The preceding digit increases by 1, but no more carries, hence, no more changes to any of the MSB digits to the left of the preceding digit
+		Claim: When 1 is added, the trail group digits all become 0
+
+		Corner Case: All 9's digits in the number. The length of the output number increases by 1, as the carry takes the MSB
+					 Ex: 999 + 1 ===> 1000
+					 
+		IN-PLACE IMPLEMENTATION as it is much more efficient
+*/
+
 #include <iostream>
 #include <stack>
 #include <climits>
 
 using namespace std;
+
+
+vector<int> plusOne(vector<int>& digits) 
+{
+	int n = digits.size();
+	
+	for (int i=n-1; i>=0; i--)
+	{		
+		// Early stop case: Carry gets passed only as long as there is a trailing group of 9's from the right. Ex" 28589999
+		// The carry stops from the current preceding digit if it is not 9. No more carries passed to the left from this digit onward.
+		if (digits[i] != 9)
+		{
+			// The digit preceding the 9 trail group
+			digits[i] = digits[i] + 1;
+			return digits;
+		}
+		
+		// Make 9 digit 0, so long as it is in the right 9 trail-group
+		digits[i] = 0;
+	}
+	
+	// If the for loop does not prematurely end, that means there is a final carry
+	// If all digits are 9, add final carry as MSB
+	digits.insert(digits.begin(), 1);
+	
+	return digits;
+}
+
+
+// // Early stop case
+// // If LSB != 9, Increment the LSB and return immediately. No problem with carry, only the LSB changes
+// if (i == n-1 && digits[i] != 9)
+// {
+// 	digits[i] = digits[i] + 1;
+// 	return digits;
+// }
 
 
 // If all the digits are 9, only then the size of the output will +1 of the input
@@ -57,42 +110,3 @@ using namespace std;
 	
 // 	return output;
 // }
-
-
-
-vector<int> plusOne(vector<int>& digits) 
-{
-	int n = digits.size();
-	
-	// Corner Case: Check if all the digits are 9, and if so the output length will be input length + 1
-	//int nine_flag = 1;
-	int sum;
-	
-	for (int i=n-1; i>=0; i--)
-	{
-		// Early stop case
-		// If LSB != 9, Increment the LSB and return immediately. No problem with carry, only the LSB changes
-		if (i == n-1 && digits[i] != 9)
-		{
-			digits[i] = digits[i] + 1;
-			return digits;
-		}
-		
-		// Early stop case: Carry gets passed only as long as there are contiguous 9's from right. Ex" 28589999
-		// The carry stops from the current digit if it is not 9. No more carries passed to the left from this digit onward.
-		if (digits[i] != 9)
-		{
-			digits[i] = digits[i] + 1;
-			return digits;
-		}
-		
-		/// Make 9 digit to 0
-		digits[i] = 0;
-	}
-	
-	// If the for loop does not prematurely end, that means there is a final carry
-	// If all digits are 9, add final carry as MSB
-	digits.insert(digits.begin(), 1);
-	
-	return digits;
-}
