ba21633569d199422b5eead729be353eb9d00be4
mvkgta26 <mvkgta26@gmail.com>
2026-06-30 17:05:14 -0400

    LC-70.cpp

---
+++ LC-70/LC-70.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+
+using namespace std;
+
+
+int climbStairs(int n) 
+{
+	// Edge case
+	if ( n == 1 )
+		return 1;
+	
+	vector<int> dp(n+1);
+	dp[1] = 1;
+	dp[2] = 2;
+	
+	for (int i=3; i <= n; i++)
+	{
+		dp[i] = dp[i-1] + dp[i-2];
+	}
+	
+	return dp[n];
+}
